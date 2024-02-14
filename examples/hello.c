482796be0e5dbeb9a25619d7172d4ea3fa9e5d17
FattiMei <teomei68@gmail.com>
2024-02-14 16:17:46 +0100

    Added all window hints and comments

--- examples/hello.c
+++ examples/hello.c
@@ -4,6 +4,11 @@
 #include <GLFW/glfw3.h>
 
 
+void log_on_error(int error, const char *description) {
+	fprintf(stderr, "GLFW error (code %d): %s\n", error, description);
+}
+
+
 void error_callback(int error, const char* description) {
 	fprintf(stderr, "GLFW Error: %s\n", description);
 }
@@ -20,34 +25,71 @@ void resize_callback(GLFWwindow *window, int width, int height) {
 }
 
 
+#define DEFAULT_WINDOW_HINTS_COUNT 36
+const int default_window_hints[DEFAULT_WINDOW_HINTS_COUNT][2] = {
+	{GLFW_RESIZABLE			, GLFW_TRUE			},
+	{GLFW_VISIBLE			, GLFW_TRUE			},
+	{GLFW_DECORATED			, GLFW_TRUE			},
+	{GLFW_FOCUSED			, GLFW_TRUE			},
+	{GLFW_AUTO_ICONIFY		, GLFW_TRUE			},
+	{GLFW_FLOATING			, GLFW_FALSE			},
+	{GLFW_MAXIMIZED			, GLFW_FALSE			},
+	{GLFW_CENTER_CURSOR		, GLFW_TRUE			},
+	{GLFW_TRANSPARENT_FRAMEBUFFER	, GLFW_FALSE			},
+	{GLFW_FOCUS_ON_SHOW		, GLFW_TRUE			},
+	{GLFW_SCALE_TO_MONITOR		, GLFW_FALSE			},
+	{GLFW_RED_BITS			, 8				},
+	{GLFW_GREEN_BITS		, 8				},
+	{GLFW_BLUE_BITS			, 8				},
+	{GLFW_ALPHA_BITS		, 8				},
+	{GLFW_DEPTH_BITS		, 24				},
+	{GLFW_STENCIL_BITS		, 8				},
+	{GLFW_ACCUM_RED_BITS		, 0				},
+	{GLFW_ACCUM_GREEN_BITS		, 0				},
+	{GLFW_ACCUM_BLUE_BITS		, 0				},
+	{GLFW_ACCUM_ALPHA_BITS		, 0				},
+	{GLFW_AUX_BUFFERS		, 0				},
+	{GLFW_SAMPLES			, 0				},
+	{GLFW_REFRESH_RATE		, GLFW_DONT_CARE		},
+	{GLFW_STEREO			, GLFW_FALSE			},
+	{GLFW_SRGB_CAPABLE		, GLFW_FALSE			},
+	{GLFW_DOUBLEBUFFER		, GLFW_TRUE			},
+	{GLFW_CLIENT_API		, GLFW_OPENGL_ES_API		},
+	{GLFW_CONTEXT_CREATION_API	, GLFW_EGL_CONTEXT_API		},
+	{GLFW_CONTEXT_VERSION_MAJOR	, 2				},
+	{GLFW_CONTEXT_VERSION_MINOR	, 0				},
+	{GLFW_CONTEXT_ROBUSTNESS	, GLFW_NO_ROBUSTNESS		},
+	{GLFW_CONTEXT_RELEASE_BEHAVIOR	, GLFW_ANY_RELEASE_BEHAVIOR	},
+	{GLFW_OPENGL_FORWARD_COMPAT	, GLFW_FALSE			},
+	{GLFW_OPENGL_DEBUG_CONTEXT	, GLFW_FALSE			},
+	{GLFW_OPENGL_PROFILE		, GLFW_OPENGL_ANY_PROFILE	}
+};
+
+
 int main() {
+	GLFWwindow *window = NULL;
 	const int width  = 800;
 	const int height = 600;
 
+	// the initialization for any glfw application
 	glfwInit();
 
-	// find the possible window hints at www.glfw.org/docs/latest/window.html
-	glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_ES_API);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
-	glfwWindowHint(GLFW_CONTEXT_CREATION_API, GLFW_EGL_CONTEXT_API);
+	// if any glfw error occurs we can catch it with this callback
+	glfwSetErrorCallback(log_on_error);
 
-	GLFWwindow* window = glfwCreateWindow(
-		  width
-		, height
-		, "hello"
-		, NULL
-		, NULL
-	);
+	// giving hints about window creation, the names are self-explanatory
+	for (int i = 0; i < DEFAULT_WINDOW_HINTS_COUNT; ++i) {
+		glfwWindowHint(default_window_hints[i][0], default_window_hints[i][1]);
+	}
+
+	// if any errors occur, it's probably the window hints
+	window = glfwCreateWindow(width, height, "hello", NULL, NULL);
 
 	if (window == NULL) {
-		fprintf(stderr, "Failed to create GLFW window\n");
 		glfwTerminate();
 		return -1;
 	}
 
-
-	glfwSetErrorCallback(error_callback);
 	glfwSetFramebufferSizeCallback(window, resize_callback);
 	glfwSetKeyCallback(window, key_callback);
 
